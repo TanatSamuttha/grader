82966c1e48da7788f1f3639a466f15f4db099612
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2026-05-18 18:24:23 +0700

    Init problems database

--- services/problems/src/server.go
+++ services/problems/src/server.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"problems/config"
 	"problems/handler"
 
 	"github.com/gofiber/fiber/v3"
 )
 
 func main() {
+	err := config.InitDatabase();
+	if err != nil {
+		panic(err);
+	}
+
 	app := fiber.New();
 
 	app.Get("/problems", func (ctx fiber.Ctx) error {
