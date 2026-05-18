fc8228956cb012b3e27cf4d3d07951dc351c5177
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2026-05-18 17:41:53 +0700

    Problems image build

--- services/problems/src/server.go
+++ services/problems/src/server.go
@@ -5,5 +5,9 @@ import "github.com/gofiber/fiber/v3"
 func main() {
 	app := fiber.New();
 
+	app.Get("/problems", func (ctx fiber.Ctx) error {
+		return ctx.SendString("Hello problems");
+	});
+
 	app.Listen(":3001");
 }
