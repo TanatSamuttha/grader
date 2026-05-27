6432f296c03e7e2dc297192c6a9bcf540de12490
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2026-05-27 22:04:28 +0700

    map web socket connection with job id

--- services/grade/src/handler/returnResult.go
+++ services/grade/src/handler/returnResult.go
@@ -1,8 +1,12 @@
 package handler
 
 import (
+	"grade/logic"
+
 	"github.com/gofiber/contrib/websocket"
 )
 
 func ReturnResult(conn *websocket.Conn) {
+	jobID := conn.Cookies("job_id");
+	logic.SocketMap[jobID] = conn;
 }
---
+++ services/grade/src/logic/websocket.go
@@ -0,0 +1,5 @@
+package logic
+
+import "github.com/gofiber/contrib/websocket"
+
+var SocketMap map[string]*websocket.Conn;
