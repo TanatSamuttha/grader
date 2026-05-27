15e748175bd033e17f19fbb78a2fc81736fefe0f
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2026-05-27 22:54:11 +0700

    Send grade result via web socket

--- services/grade/src/logic/websocket.go
+++ services/grade/src/logic/websocket.go
@@ -1,6 +1,9 @@
 package logic
 
 import (
+	"encoding/json"
+	"grade/models"
+	"log"
 	"sync"
 
 	"github.com/gofiber/contrib/websocket"
@@ -9,3 +12,24 @@ import (
 var SocketMap map[string]*websocket.Conn;
 
 var SocketMutex sync.RWMutex
+
+var GradeResBuffer chan models.GradeResDTO = make(chan models.GradeResDTO);
+
+func SendResult(GradeResBuffer <- chan models.GradeResDTO) {
+	for res := range GradeResBuffer {
+		jobID := res.JobID;
+
+		SocketMutex.Lock();
+		conn := SocketMap[jobID];
+		SocketMutex.Unlock();
+
+		res.JobID = "";
+
+		resJson, err := json.Marshal(res);
+		if err != nil {
+			log.Println("Error json marshal -> " + err.Error());
+		}
+
+		conn.WriteMessage(websocket.TextMessage, resJson);
+	}
+}
--- services/grade/src/models/models.go
+++ services/grade/src/models/models.go
@@ -1,36 +1,43 @@
 package models
 
 type Problem struct {
-	ID				uint32				`gorm:"primaryKey"`
-	ProblemID		string				`gorm:"unique"`
-	Name			string
-	AuthorUID		string
-	TestCasesSize	uint8
-	TimeLimit		uint8
-	MemoryLimit		uint8
-	Visibility		string
+	ID            uint32 `gorm:"primaryKey"`
+	ProblemID     string `gorm:"unique"`
+	Name          string
+	AuthorUID     string
+	TestCasesSize uint8
+	TimeLimit     uint8
+	MemoryLimit   uint8
+	Visibility    string
 }
 
 type User struct {
-	ID				uint32 				`gorm:"primaryKey"`
-	UID 			string 				`gorm:"unique"`
-	Google_UID 		string
-	Email 			string
-	Username 		string
-	PhotoURL 		string
-	Role			string
-	Version 		uint16
+	ID         uint32 `gorm:"primaryKey"`
+	UID        string `gorm:"unique"`
+	Google_UID string
+	Email      string
+	Username   string
+	PhotoURL   string
+	Role       string
+	Version    uint16
 }
 
 type Job struct {
-	ID				string
-	UID				string
-	ProblemID		string
-	Code			string
-	Lang			string
+	ID        string
+	UID       string
+	ProblemID string
+	Code      string
+	Lang      string
 }
 
 type CodeDTO struct {
-	Code			string				`json:"code"`
-	Lang			string				`json:"lang"`
+	Code string `json:"code"`
+	Lang string `json:"lang"`
+}
+
+type GradeResDTO struct {
+	JobID	string
+	Task 	int
+	Result 	bool
+	Error	string
 }
